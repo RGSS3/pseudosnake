2881e5c4c717df2cd00d19196d3992465c6fc02b
seiran <924172963@qq.com>
2019-11-09 17:18:10 +0800

    Create pseudo.py

---
+++ pseudo.py
@@ -0,0 +1,24 @@
+
+def lam(f):
+    x = { name : value.cell_contents  for name, value in zip(f.__code__.co_freevars, list(f.__closure__ or []))}
+    g = dict(f.__globals__ or {})
+    g.update(x)
+    x['__lam__'] = []
+    
+    exec("""def fn""" + f() + """\n__lam__.append(fn)""", g, x)
+    return x['__lam__'][0]
+
+def capture(a, b):
+    return b
+
+def run():
+    a = lam(lambda: """(a):
+            print("Currying a")
+            return lam(lambda: capture(a, 
+                    \"""(b):
+                        print(f"curried a is {a}")
+                        return a + b\"""))
+        """)
+    print(a(5)(7))
+    
+run()
